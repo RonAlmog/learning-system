acd1237300c9954ad3c61de5266c5dd51dc8a7e0
Ron Almog <ron8132@gmail.com>
2023-10-04 14:44:18 -0400

    add access control to chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,12 +1,13 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import { redirect } from "next/dist/server/api-utils";
 import Link from "next/link";
 import ChapterTitleForm from "./_components/chapter-title-form";
 import ChapterDescriptionForm from "./_components/chapter-description-form";
 import { NextResponse } from "next/server";
+import ChapterAccessForm from "./_components/chapter-access-form";
 //import { useRouter } from "next/navigation";
 
 const ChapterPage = async ({
@@ -75,6 +76,17 @@ const ChapterPage = async ({
             chapterId={params.chapterId}
           />
           <ChapterDescriptionForm
+            initialData={chapter}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+          />
+        </div>
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={Eye} />
+            <h2 className="text-xl">Access Settings</h2>
+          </div>
+          <ChapterAccessForm
             initialData={chapter}
             courseId={params.courseId}
             chapterId={params.chapterId}
