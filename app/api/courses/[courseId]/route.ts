4a8e3d4e6e9356388f5a74eb76dcc14bd4198f6f
Ron Almog <ron8132@gmail.com>
2023-10-06 13:14:00 -0400

    add delete course, with chapters and videos

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,7 +1,62 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
+import { Chapter } from "@prisma/client";
 
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!
+);
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Uauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+      include: {
+        chapters: {
+          include: { muxData: true },
+        },
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Not found", { status: 404 });
+    }
+
+    try {
+      for (const chapter of course.chapters) {
+        if (chapter.muxData?.assetId) {
+          await Video.Assets.del(chapter.muxData.assetId);
+        }
+      }
+    } catch (err) {
+      console.log("error deleting video file");
+    }
+
+    const deletedCourse = await db.course.delete({
+      where: {
+        id: params.courseId,
+      },
+    });
+
+    return NextResponse.json(deletedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string } }
