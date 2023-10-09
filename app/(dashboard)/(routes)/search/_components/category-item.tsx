2c1a5a74a6a516c5d449999e1e27171aa631e5d0
Ron Almog <ron8132@gmail.com>
2023-10-08 21:17:13 -0400

    keep state of menu items with qs

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -2,7 +2,8 @@
 
 import { cn } from "@/lib/utils";
 import { IconType } from "react-icons";
-
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 interface CategoryItemProps {
   label: string;
   value?: string;
@@ -10,10 +11,38 @@ interface CategoryItemProps {
 }
 
 const CategoryItem = ({ label, value, icon: Icon }: CategoryItemProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("categoryId");
+  const currentTitle = searchParams.get("title");
+  // is this a function?
+  const isSelected = currentCategoryId === value;
+
+  const onClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: currentTitle,
+          categoryId: isSelected ? null : value,
+        },
+      },
+      {
+        skipNull: true,
+        skipEmptyString: true,
+      }
+    );
+    router.push(url);
+  };
+
   return (
     <button
+      onClick={onClick}
       className={cn(
-        "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition"
+        "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",
+        isSelected && "border-sky-700 bg-sky-200/20 text-sky-800"
       )}
       type="button"
     >
