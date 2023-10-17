cccc6eff84bd470c1c8efcd11db7576a30c4a700
Ron Almog <ron8132@gmail.com>
2023-10-17 15:41:36 -0400

    fix sidebar bug

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -57,6 +57,11 @@ const ChapterIdPage = async ({
             completeOnEnd={completeOnEnd}
           />
         </div>
+        <div>
+          <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+            <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -47,10 +47,10 @@ const CourseLayout = async ({
   return (
     // i added flex Here...
     <div className="h-full">
-      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50 border-red-700">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
         <CourseNavbar course={course} progressCount={progressCount} />
       </div>
-      <div className="hidden md:flex h-full w-80 flex-col inset-y-0 z-50">
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
         <CourseSidebar course={course} progressCount={progressCount} />
       </div>
       <main className="md:pl-80 h-full pt-[80px]">{children}</main>
