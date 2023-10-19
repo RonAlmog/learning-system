a4961cedb4accbf913d91e25f3c625708f05b4ba
Ron Almog <ron8132@gmail.com>
2023-10-19 08:43:13 -0400

    add action: get dashboard courses

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Dashboard() {
+  return <div></div>;
+}
